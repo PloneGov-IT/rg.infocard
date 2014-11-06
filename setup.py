5d305f54d0b735ff56383a5d24eb6afef53f16f3
Alessandro Pisa <alessandro.pisa@redturtle.it>
2014-11-06 11:16:10 +0100

    Back to development: 0.9.2

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ long_description = "\n\n".join((
 
 setup(
     name='rg.infocard',
-    version='0.9.1',
+    version='0.9.2.dev0',
     description="rg.infocard",
     long_description=long_description,
     # Get more from http://pypi.python.org/pypi?%3Aaction=list_classifiers
